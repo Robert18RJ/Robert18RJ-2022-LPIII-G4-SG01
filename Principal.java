e702a88eee233dd5e2386d30abc5042ccc5355fd
Robert Dueñas <robertduenas@ucsm.edu.pe>
2022-08-20 20:32:02 -0500

    Premio Laptop

--- Principal.java
+++ Principal.java
@@ -17,7 +17,8 @@ public class Principal {
 
         } while (vida < 3 && !fl);
         if (fl == true)
-            System.out.println("Adivinaste!!!");            
+            System.out.println("Adivinaste!!!");
+            System.out.println("Ganaste una Laptop");            
         else
             System.out.println("Ups, perdiste...");
 
