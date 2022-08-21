6e7e0ac9e2836ecbaaf125ea8d886cf737097139
Robert Dueñas <robertduenas@ucsm.edu.pe>
2022-08-20 20:22:59 -0500

    Principal

---
+++ Principal.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int vida, rp;
+        boolean fl = false;
+        vida = 0;
+
+        do {
+            vida++;
+            System.out.println("Vida:" + vida + "\t¿Cual es el numero secreto? ");
+            rp = sc.nextInt();
+            if (rp == 1234)
+                fl = true;
+
+        } while (vida < 3 && !fl);
+        if (fl == true)
+            System.out.println("Adivinaste!!!");            
+        else
+            System.out.println("Ups, perdiste...");
+
+        sc.close();
+    }
+}
