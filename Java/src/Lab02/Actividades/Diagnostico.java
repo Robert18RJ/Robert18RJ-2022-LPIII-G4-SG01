50e18277abbca2324801334c51c12e73a25b44f8
Robert Dueñas <robertduenas@ucsm.edu.pe>
2022-08-26 23:46:21 -0500

    Implementacion class Diagnostico

---
+++ Java/src/Lab02/Actividades/Diagnostico.java
@@ -0,0 +1,17 @@
+package Lab02.Actividades;
+public class Diagnostico {
+
+    public static String aptoVacunaA(Perro mascota) {
+        if (mascota.getTemp() > 36 && mascota.getTemp() < 41 && mascota.getEdad() > 1 && mascota.peso > 5)
+            return "El Perro esta SANO";
+        else
+            return "El Perro esta ENFERMO";
+    }
+
+    public static String aptoVacunaB(Perro mascota) {
+        if (mascota.getTemp() > 36 && mascota.getTemp() < 41 && mascota.getEdad() > 1 && mascota.peso > 5)
+            return "El Perro esta SANO";
+        else
+            return "El Perro esta ENFERMO";
+    }
+}
