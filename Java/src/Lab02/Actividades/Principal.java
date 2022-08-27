b4ed15c58bca50d7c02b50d633333bbd797a123e
Robert Dueñas <robertduenas@ucsm.edu.pe>
2022-08-26 23:51:26 -0500

    Ingreso de datos del usuario (peso,edad,temp)

--- Java/src/Lab02/Actividades/Principal.java
+++ Java/src/Lab02/Actividades/Principal.java
@@ -1,18 +1,23 @@
 package Lab02.Actividades;
 import java.util.Scanner;
-
 public class Principal {
     public static void main(String[] args) {    
-        Scanner entrada =new Scanner(System.in);         
-        
+        Scanner entrada =new Scanner(System.in);
         Perro Fido = new Perro("chihuahua", "marron");
-        Perro Pelusa = new Perro("pequines", "blanco");
-        System.out.println(Fido.toString());
-        System.out.println(Pelusa.toString());
-        Fido.increEdad(2);
-        Pelusa.increEdad(5);
-        System.out.println(Fido.toString());
-        System.out.println(Pelusa.toString());        
+        int edad;
+        Double peso,temperatura;      
+        System.out.println("Ingrese el nuevo peso para el perro : ");
+        peso=entrada.nextDouble();
+        Fido.peso=peso;
+        System.out.println("Ingrese la nueva edad para el perro : ");
+        edad=entrada.nextInt();
+        Fido.SetEdad(edad);
+        System.out.println("Ingrese la nueva temperatura para el perro : ");
+        temperatura=entrada.nextDouble();
+        Fido.SetTemp(temperatura);
+        System.out.println("DIAGNOSTICO: " + Diagnostico.aptoVacunaB(Fido));
+        System.out.println(Fido.toString());      
         entrada.close();
     }
-}
+}   
+
